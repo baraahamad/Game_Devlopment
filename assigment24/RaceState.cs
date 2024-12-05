5fa8b655aacb6bebac14f1ec8d68d2aba0b8788a
baraahamad <118076703+baraahamad@users.noreply.github.com>
2024-12-05 16:07:44 +0200

    Add files via upload

---
+++ assigment24/RaceState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// RaceState.cs
+public enum RaceState
+{
+    Start,
+    Accelerate,
+    Turn,
+    Crash,
+    Finish
+}
+
